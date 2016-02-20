b19ab73d1411c5af7368511cd9baf83ce2e09067
Pereverzev-Alexander <sanya.home01@gmail.com>
2016-02-20 19:35:02 +0300

    Updated files from the master

--- src/entites/GeneralEntity.java
+++ src/entites/GeneralEntity.java
@@ -2,9 +2,12 @@ package entites;
 
 /**
  * @author Alexander
- * Сущность для таблицы "Информация о заявке"
+ * Общая сущность, включающая в себя все поля. Используется для экспорта в XLS документ и 
+ * для запроса, возвращающего ВСЮ информацию о заявке 
  */
-public class Info_request {
+public class GeneralEntity {
+	// Тип подключаемой услуги (Интернет/ТВ/Телефон)
+	private String typeService;
 	// Дата регистрации заявки
 	private long incomeDate;
 	// ФИО клиента
@@ -17,67 +20,116 @@ public class Info_request {
 	private long closedDate;
 	// Комментарий к заявке
 	private String comment;
+	// ФИО оператора, принявшего заявку
+	private String fullNameOperator = null;
+	// ФИО мастера
+	private String fullNameMaster = null;
 	
 	
-	public Info_request(){
+	public GeneralEntity(){
+		typeService = null;
 		incomeDate = 0;
 		fullNameClient = null;
 		address = null;
 		serviceDate = 0;
 		closedDate = 0;
 		comment = null;
+		fullNameOperator = null;
+		fullNameMaster = null;
 	}
-		
-	public Info_request(
+	
+	public GeneralEntity(
+			String _typeService,
 			long _incomeDate,
 			String _fullNameClient,
 			String _address,
 			long _serviceDate,
 			long _closedDate,
-			String _comment){
+			String _comment,
+			String _fullNameOperator,
+			String _fullNameMaster){
+		typeService = _typeService;
 		incomeDate = _incomeDate;
 		fullNameClient = _fullNameClient;
 		address = _address;
 		serviceDate = _serviceDate;
 		closedDate = _closedDate;
 		comment = _comment;
+		fullNameOperator = _fullNameOperator;
+		fullNameMaster = _fullNameMaster;
 	}
-	
+
+	public String getTypeService() {
+		return typeService;
+	}
+
+	public void setTypeService(String _typeService) {
+		typeService = _typeService;
+	}
+
 	public long getIncomeDate() {
 		return incomeDate;
 	}
+
 	public void setIncomeDate(long _incomeDate) {
 		incomeDate = _incomeDate;
 	}
+
 	public String getFullNameClient() {
 		return fullNameClient;
 	}
+
 	public void setFullNameClient(String _fullNameClient) {
 		fullNameClient = _fullNameClient;
 	}
+
 	public String getAddress() {
 		return address;
 	}
+
 	public void setAddress(String _address) {
 		address = _address;
 	}
+
 	public long getServiceDate() {
 		return serviceDate;
 	}
+
 	public void setServiceDate(long _serviceDate) {
 		serviceDate = _serviceDate;
 	}
+
 	public long getClosedDate() {
 		return closedDate;
 	}
+
 	public void setClosedDate(long _closedDate) {
 		closedDate = _closedDate;
 	}
+
 	public String getComment() {
 		return comment;
 	}
+
 	public void setComment(String _comment) {
 		comment = _comment;
 	}
-		
+
+	public String getEmployeePosition() {
+		return fullNameOperator;
+	}
+
+	public void setEmployeePosition(String _fullNameOperator) {
+		fullNameOperator = _fullNameOperator;
+	}
+
+	public String getFullNameEmployee() {
+		return fullNameMaster;
+	}
+
+	public void setFullNameEmployee(String _fullNameMaster) {
+		fullNameMaster = _fullNameMaster;
+	}
+	
+	
 }
