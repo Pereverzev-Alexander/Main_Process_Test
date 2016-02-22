3f0429aa9fa165d20bf0af67d57fc580505830de
Denis Murashko <denismursfr@mail.ru>
2016-02-23 00:28:48 +0300

    draw borders

--- src/convertUtilites/ConvertToXLS.java
+++ src/convertUtilites/ConvertToXLS.java
@@ -13,6 +13,7 @@ import java.util.Set;
 import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellStyle;
 import org.apache.poi.ss.usermodel.Row;
 
 import entities.Request;
@@ -23,7 +24,21 @@ public class ConvertToXLS {
 
 		HSSFWorkbook workbook = new HSSFWorkbook();
 		HSSFSheet sheet = workbook.createSheet("GeneralEntitys");
-
+		//thin lines
+		CellStyle style = workbook.createCellStyle();
+		style.setBorderBottom(CellStyle.BORDER_THIN);
+		style.setBorderRight(CellStyle.BORDER_THIN);
+		style.setBorderLeft(CellStyle.BORDER_THIN);
+		style.setBorderTop(CellStyle.BORDER_THIN);
+		//thick lines
+		CellStyle style_thick = workbook.createCellStyle();
+		style_thick.setBorderBottom(CellStyle.BORDER_THICK);
+		style_thick.setBorderRight(CellStyle.BORDER_THICK);
+		style_thick.setBorderLeft(CellStyle.BORDER_THICK);
+		style_thick.setBorderTop(CellStyle.BORDER_THICK);
+		
+		
+		
 		Map<String, Object[]> data = new HashMap<String, Object[]>();
 		//create heading
 		data.put(Integer.toString(1), new Object[] {"Дата регистрации заявки",
@@ -56,6 +71,12 @@ public class ConvertToXLS {
 			int cellnum = 0;
 			for (Object obj : objArr) {
 				Cell cell = row.createCell(cellnum++);
+				//draw border
+				if (rownum==1)
+					cell.setCellStyle(style_thick);
+				else
+				cell.setCellStyle(style);
+				//types of cells
 				if(obj instanceof Date) 
 					cell.setCellValue((Date)obj);
 				else if(obj instanceof Boolean)
@@ -69,6 +90,7 @@ public class ConvertToXLS {
 				else if(obj instanceof Double)
 					cell.setCellValue((Double)obj);
 			}
+			
 		}
 
 		//Get iterator to all the rows in current sheet
@@ -77,6 +99,8 @@ public class ConvertToXLS {
 		//Iterator<Cell> cellIterator = row.cellIterator();
 		//int rownum = sheet.getLastRowNum();
 		
+		
+        
 		int countColumns = 10;
 		for (int i=0;i<countColumns;i++){
 			sheet.autoSizeColumn(i);
